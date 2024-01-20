a17abf1e2eb4dbfc4bbc2d81df95df8f7edbf3c7
esalu-dev <esalu88@outlook.com>
2024-01-19 19:55:32 -0600

    Update API key handling in sendEmail function

--- src/services/sendEmail.ts
+++ src/services/sendEmail.ts
@@ -5,13 +5,14 @@ interface Email {
 }
 
 export async function sendEmail(body: Email, apiKey: string) {
+  const API_KEY = process.env.API_KEY;
   const url = "https://api.brevo.com/v3/smtp/email";
   const options = {
     method: "POST",
     headers: {
       accept: "application/json",
       "content-type": "application/json",
-      "api-key": apiKey,
+      "api-key": apiKey ?? API_KEY,
     },
     body: JSON.stringify({
       sender: { name: "Portfolio Notification", email: "esalu88@outlook.com" },
